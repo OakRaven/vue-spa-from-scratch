880f717bf641f1e80edac2fae5d72a806b5abf0c
Michael Hatfield <mike.hatfield@nicom.local>
2017-10-23 12:29:42 -0300

    Completed State Management module

--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -2,8 +2,6 @@
   <div class="content">
     <div v-if="isAuthenticated">
       Hello authenticated user!
-      <p>Name: {{profile.firstName}}</p>
-      <p>Favourite Sandwich: {{profile.favoriteSandwich}}</p>
       <button v-on:click="logout()" class="button is-primary">
         Logout
       </button>
@@ -53,55 +51,31 @@
 </template>
 
 <script>
-  import appService from '../app.service'
+  import { mapGetters, mapActions } from 'vuex'
 
   export default {
     data () {
       return {
         username: '',
-        password: '',
-        isAuthenticated: false,
-        profile: {}
+        password: ''
       }
     },
 
-    watch: {
-      isAuthenticated: function (val) {
-        if (val) {
-          appService.getProfile()
-            .then(profile => {
-              this.profile = profile
-            })
-        } else {
-          this.profile = {}
-        }
-      }
+    computed: {
+      ...mapGetters(['isAuthenticated'])
     },
 
     methods: {
+      ...mapActions({
+        logout: 'logout'
+      }),
+
       login () {
-        appService.login({username: this.username, password: this.password})
-          .then((data) => {
-            window.localStorage.setItem('token', data.token)
-            window.localStorage.setItem('tokenExpiration', data.expiration)
-            this.isAuthenticated = true
+        this.$store.dispatch('login', {username: this.username, password: this.password})
+          .then(() => {
             this.username = ''
             this.password = ''
           })
-          .catch(() => window.alert('Count not login!'))
-      },
-
-      logout () {
-        window.localStorage.setItem('token', null)
-        window.localStorage.setItem('tokenExpiration', null)
-        this.isAuthenticated = false
-      }
-    },
-    created () {
-      let expiration = window.localStorage.getItem('tokenExpiration')
-      var unixTimestamp = new Date().getTime() / 1000
-      if (expiration !== null && parseInt(expiration) - unixTimestamp > 0) {
-        this.isAuthenticated = true
       }
     }
   }
